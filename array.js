f85b5f1549dd2e0318c154ccbf70e3708b480b00
Krishnakumar3k <krishnakumar3kgt@gmail.com>
2023-08-08 01:39:38 +0530

    intialcommit

---
+++ array.js
@@ -0,0 +1,12 @@
+/* let array = ["krishna", 25, 5, "LCO", true]
+console.log(array);
+console.log(array[1]);
+console.log(array.length); */
+
+let userName = "krishna@.com";
+let full_Name = "Krishna";
+let Phone_Num = "6201654835";
+let pass = "kk@###*";
+// console.log("My Username is " +userName + " My contact number "+ Phone_Num) ;
+console.log(`My username is: ${userName}
+My contact Number ist: ${Phone_Num}`);
---
+++ es6.js
@@ -0,0 +1,8 @@
+// ES6
+// Rest and Spred Operator
+
+function addNumbers(a,b,c){
+return a+b+c;
+}
+let sum = addNumbers(2, 4, 5);
+console.log(sum);
